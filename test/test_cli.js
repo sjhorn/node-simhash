422b2620fdd664c213149420bab95d579d849695
Scott Horn <shorn@expedia.com>
2016-06-05 11:31:14 +1000

    Added unit tests

---
+++ test/test_cli.js
@@ -0,0 +1,32 @@
+var exec = require('child_process').exec;
+
+exports['Cli shows Help'] = function(test) {
+    exec('node lib/cli.js -h', function(error, stdout, stderr) {
+        if (error != null) {
+            test.ok(false, "cli does not successfully show help");
+        }
+        test.done();
+    })
+}
+
+exports['Cli Compare Files'] = function(test) {
+    exec('node lib/cli.js test/sample1.txt test/sample2.txt', function(error, stdout, stderr) {
+        if (error != null) {
+            test.ok(false, "cli does not successfully compare files");
+        } else {
+            test.ok(stdout.indexOf("Similarity is") != -1, "Failed to display summary");
+        }
+        test.done();
+    })
+}
+
+exports['Cli Compare sites'] = function(test) {
+    exec('node lib/cli.js https://raw.githubusercontent.com/sjhorn/node-simhash/master/LICENCE https://raw.githubusercontent.com/sjhorn/node-simhash/master/LICENCE', function(error, stdout, stderr) {
+        if (error != null) {
+            test.ok(false, "cli does not successfully compare pages");
+        } else {
+            test.ok(stdout.indexOf("Similarity is") != -1, "Failed to display summary");
+        }
+        test.done();
+    })
+}
---
+++ test/test_simhash.js
@@ -0,0 +1,20 @@
+var simhash = require('../lib/simhash.js');
+var fs = require('fs');
+
+exports.compareSmallString = function(test) {
+    test.equal(simhash.compare('I am working', 'I am working'), 1);
+    test.done();
+}
+
+exports.compareSameFile = function(test) {
+    var file1 = fs.readFileSync("test/sample1.txt", "utf8");
+    test.equal(simhash.compare(file1, file1), 1);
+    test.done();
+}
+
+exports.compareSimilarFile = function(test) {
+    var file1 = fs.readFileSync("test/sample1.txt", "utf8");
+    var file2 = fs.readFileSync("test/sample2.txt", "utf8");
+    test.equal(simhash.compare(file1, file2), 0.6875);
+    test.done();
+}
